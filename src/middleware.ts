c1a440139ed725abb58f2c6fb90276bf8c0bb920
omarwebacademy <oelbaz020@gmail.com>
2026-02-17 21:07:31 +0000

    fix: adjust middleware to allow static images

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,12 @@ import { routing } from './i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+    matcher: [
+        // Match all pathnames EXCEPT for:
+        // - /api (API routes)
+        // - /_next (Next.js internals)
+        // - /_vercel (Vercel internals)
+        // - all files with an extension (e.g. favicon.ico, image.jpg)
+        '/((?!api|_next|_vercel|.*\\..*).*)'
+    ]
 };
